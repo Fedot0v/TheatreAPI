51d14f569a11ed12e650a2fb7739b063ebe1981e
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-15 15:35:35 +0300

    create ImageUploadMixin

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ImageUploadMixin:
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading an image."""
+        obj = self.get_object()
+        serializer = self.get_serializer(obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
