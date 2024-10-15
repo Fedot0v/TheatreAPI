3f86e34dfe2b224659ee7cd3fb2747d1b9dc8de1
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-15 15:37:27 +0300

    create genre, actor, play views

--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+
+from theatre.models import Play, Genre, Actor
+from theatre.serializers import (
+    PlayDetailSerializer,
+    PlaySerializer,
+    PlayImageSerializer,
+    GenreSerializer,
+    GenreDetailSerializer,
+    ActorSerializer,
+    ActorDetailSerializer,
+    ActorImageSerializer
+)
 
 
 class ImageUploadMixin:
@@ -24,3 +36,79 @@ class ImageUploadMixin:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GenreDetailSerializer
+
+        return GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet, ImageUploadMixin):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return ActorDetailSerializer
+
+        if self.action == "upload_image":
+            return ActorImageSerializer
+
+        return ActorSerializer
+
+    def get_queryset(self):
+        """Retrieve the actors with filters"""
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = self.queryset
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset.distinct()
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+    def get_queryset(self):
+        """Retrieve the plays with filters"""
+        title = self.request.query_params.get("title")
+        actors = self.request.query_params.get("actors")
+        genres = self.request.query_params.get("genres")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if actors:
+            actors_ids = params_to_int(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        if genres:
+            genres_ids = params_to_int(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        if self.action == "upload_image":
+            return PlayImageSerializer
+
+        return PlaySerializer
