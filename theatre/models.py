fb05062b1ffd6ffef2f6a3f9d91179799b33a217
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-15 15:07:10 +0300

    create performance and ticket models

--- theatre/models.py
+++ theatre/models.py
@@ -2,7 +2,10 @@ import os
 import uuid
 
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.template.defaultfilters import slugify
+
+from TheatreAPIService import settings
 
 
 def create_custom_path(instance, filename):
@@ -50,3 +53,66 @@ class Play(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class Performance(models.Model):
+    play: Play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performances",
+    )
+    theatre_hall: TheatreHall = models.ForeignKey(
+        TheatreHall,
+        on_delete=models.CASCADE,
+        related_name="performances",
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"{self.play}."
+                f" Theatre hall: {self.theatre_hall}."
+                f" Show time: {self.show_time}"
+        )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["row", "seat", "performance"],
+                name="unique_ticket"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.performance}. Seat: {self.seat}, row: {self.row}"
