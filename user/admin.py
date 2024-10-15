19df5eab70acf10e8ba3a94826649355069241b2
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-15 15:51:04 +0300

    create admin and views

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model without groups and user_permissions fields."""
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Permissions"), {
+            "fields": ("is_active", "is_staff", "is_superuser")
+        }),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+    list_display = ("email", "is_staff")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    filter_horizontal = ()
+    list_filter = ("is_staff", "is_superuser")
